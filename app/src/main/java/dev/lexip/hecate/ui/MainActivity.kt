af3d5aa6795ab257c1012ca0369ecb08e4e80f6c
xLexip <x@lexip.dev>
2025-10-30 20:02:40 +0100

    #45: feat(ui): Implement permission-error dialog
    * Dialog for when the WRITE_SECURE_SETTINGS permission is missing.
    * Provide the ADB command (and a copy action).

--- app/src/main/java/dev/lexip/hecate/ui/MainActivity.kt
+++ app/src/main/java/dev/lexip/hecate/ui/MainActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 xLexip <https://lexip.dev>
+ * Copyright (C) 2024-2025 xLexip <https://lexip.dev>
  *
  * Licensed under the GNU General Public License, Version 3.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,12 +12,17 @@
 
 package dev.lexip.hecate.ui
 
+import android.Manifest
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.lexip.hecate.HecateApplication
@@ -42,11 +47,32 @@ class MainActivity : ComponentActivity() {
 				)
 			)
 			val state by adaptiveThemeViewModel.uiState.collectAsState()
+
+			val hasPermission = hasWriteSecureSettingsPermission()
+			val copyAdbCommand: (String) -> Unit =
+				{ adbCommand -> copyToClipboard("ADB Command", adbCommand) }
+
 			HecateTheme {
-				AdaptiveThemeScreen(state, adaptiveThemeViewModel::updateAdaptiveThemeEnabled)
+				AdaptiveThemeScreen(
+					state,
+					adaptiveThemeViewModel::updateAdaptiveThemeEnabled,
+					hasPermission,
+					copyAdbCommand
+				)
 			}
 		}
 
 	}
-}
 
+	private fun hasWriteSecureSettingsPermission(): Boolean {
+		val permission = Manifest.permission.WRITE_SECURE_SETTINGS
+		return ContextCompat.checkSelfPermission(this, permission) == PERMISSION_GRANTED
+	}
+
+	private fun copyToClipboard(label: String, text: String) {
+		val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+		val clip = ClipData.newPlainText(label, text)
+		clipboard.setPrimaryClip(clip)
+	}
+
+}
