086e7fdbe3cc2b98353d0a80b5e2ca758bc8acd7
xLexip <x@lexip.dev>
2025-10-31 04:28:41 +0100

    feat: Integrate Firebase Crashlytics
    #7

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 	alias(libs.plugins.kotlin.android) apply false
 	alias(libs.plugins.kotlin.compose) apply false
 	id("org.sonarqube") version "7.0.1.6134"
+	id("com.google.gms.google-services") version "4.4.4" apply false
+	alias(libs.plugins.google.firebase.crashlytics) apply false
 }
 
 sonar {
