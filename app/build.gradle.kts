477a5abb2e6255f5a64bbdfa5533972eb3e2de0b
xLexip <x@lexip.dev>
2025-12-05 20:00:41 +0100

    build: Enable optimized resource shrinking & NDK debug symbols

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ android {
 				getDefaultProguardFile("proguard-android-optimize.txt"),
 				"proguard-rules.pro"
 			)
+			ndk {
+				debugSymbolLevel = "SYMBOL_TABLE"
+			}
 		}
 	}
 	compileOptions {
