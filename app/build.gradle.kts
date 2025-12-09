9e57c9a6f04cd40929f6bcf2dc60bcb5fd60869b
xLexip <x@lexip.dev>
2025-12-09 15:08:23 +0100

    docs: Update README

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
 		applicationId = "dev.lexip.hecate"
 		minSdk = 31
 		targetSdk = 36
-		versionCode = 36
-		versionName = "0.7.0"
+		versionCode = 46
+		versionName = "0.9.0"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 
