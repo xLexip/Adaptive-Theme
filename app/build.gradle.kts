e3f0a4de6ccaa634174421fbfbd416c9ce6fb945
xLexip <x@lexip.dev>
2025-12-15 21:19:30 +0100

    build: v0.10.1-beta
    * Patch release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
 		applicationId = "dev.lexip.hecate"
 		minSdk = 34
 		targetSdk = 36
-		versionCode = 60
-		versionName = "0.10.0"
+		versionCode = 63
+		versionName = "0.10.1"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 
