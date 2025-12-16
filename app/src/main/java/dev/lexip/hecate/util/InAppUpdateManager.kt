8ba55d889afd626188b2bd877781e006b4786469
xLexip <x@lexip.dev>
2025-12-16 18:08:34 +0100

    refactor(util): Show flexible update prompts instantly
    * Immediate update prompts after 3 days

--- app/src/main/java/dev/lexip/hecate/util/InAppUpdateManager.kt
+++ app/src/main/java/dev/lexip/hecate/util/InAppUpdateManager.kt
@@ -32,7 +32,7 @@ private const val TAG = "InAppUpdateManager"
 
 private const val DAYS_FOR_IMMEDIATE_UPDATE = 3
 private const val MIN_PRIORITY_FOR_IMMEDIATE = 0
-private const val DAYS_FOR_FLEXIBLE_UPDATE = 1
+private const val DAYS_FOR_FLEXIBLE_UPDATE = 0
 private const val MIN_PRIORITY_FOR_FLEXIBLE = 0
 
 class InAppUpdateManager(activity: ComponentActivity) {
