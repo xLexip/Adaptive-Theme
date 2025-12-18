6c4d937d74b74bec1bbf6cd81d52c8ccce309f11
Lexip <x@lexip.dev>
2025-12-18 20:50:05 +0100

    chore(analytics): Push analytics gate
    * Disables analytics when not installed from Google Play
    * Disables analytics on debug builds
    * Excludes certain test devices
    * Secrets moved elsewhere

---
+++ app/src/main/java/dev/lexip/hecate/analytics/AnalyticsGate.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2025 xLexip <https://lexip.dev>
+ *
+ * Licensed under the GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.gnu.org/licenses/gpl-3.0
+ *
+ * Please see the License for specific terms regarding permissions and limitations.
+ */
+
+package dev.lexip.hecate.analytics
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.provider.Settings
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import dev.lexip.hecate.BuildConfig
+import dev.lexip.hecate.util.InstallSourceChecker
+
+object AnalyticsGate {
+  
+  private val EXCLUDED_ANDROID_IDS = listOf(
+		"4840d1ef56c9ab8"
+	)
+  
+	@Volatile
+	private var analyticsEnabled = false
+
+	@Volatile
+	private var crashlyticsEnabled = false
+
+	@Volatile
+	private var isPlayStoreInstall = false
+
+	@SuppressLint("HardwareIds")
+	fun init(context: Context) {
+		isPlayStoreInstall = InstallSourceChecker.isInstalledFromPlayStore(context)
+
+		val isDebug = BuildConfig.DEBUG
+		val androidId =
+			Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+		val isExcludedDevice = androidId in EXCLUDED_ANDROID_IDS
+
+		analyticsEnabled = !isDebug && isPlayStoreInstall && !isExcludedDevice
+		crashlyticsEnabled = !isDebug && isPlayStoreInstall
+
+		// Apply settings to Firebase
+		FirebaseAnalytics.getInstance(context).setAnalyticsCollectionEnabled(analyticsEnabled)
+		FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled = crashlyticsEnabled
+	}
+
+	fun allowed(): Boolean = analyticsEnabled
+}
