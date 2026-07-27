5da9c56fee83f61a0fc771e027c1a4da9ff62e27
xLexip <x@lexip.dev>
2026-07-27 11:18:58 +0200

    chore(deps): Convert gms dependency to alias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	id("com.google.gms.google-services")
+	alias(libs.plugins.google.services)
 	alias(libs.plugins.android.application)
 	alias(libs.plugins.kotlin.android)
 	alias(libs.plugins.kotlin.compose)
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 	alias(libs.plugins.android.application) apply false
 	alias(libs.plugins.kotlin.android) apply false
 	alias(libs.plugins.kotlin.compose) apply false
-	id("com.google.gms.google-services") version "4.5.0" apply false
+	alias(libs.plugins.google.services) apply false
 	alias(libs.plugins.google.firebase.crashlytics) apply false
 }
 
